2da7ca9468b56c9bf07a9143e8c0070ef0c75183
Pierre Chesne <pierrc@MacBook-Pro-de-Pierre.local>
2022-04-07 16:20:14 +0200

    artifact

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,19 +1,12 @@
 variable "azureRegion" {
   description = "Azure Region where to deploy resources. Caution the region must support Availability Zone"
-  # To get names of Azure Region : az account list-locations
-  # To check support of Availability Zone in the Azure Region see https://docs.microsoft.com/bs-latn-ba/azure/availability-zones/az-overview
-  type    = string
   default = "westeurope"
 }
 
-#  Resource Group Name
 variable "resourceGroupName" {
   type    = string
   default = "RG-DemoGitHub"
 }
-
-
-# Variables pour l'authentification du provider Terraform  
 
 variable "AzureSubscriptionID" {
   type = string
--- Terraform/version.tf
+++ Terraform/version.tf
@@ -1,47 +1,22 @@
-#   _______                   __                                           _                                              _     _               
-#  |__   __|                 / _|                                         (_)               ___                          (_)   | |              
-#     | | ___ _ __ _ __ __ _| |_ ___  _ __ _ __ ___    __   _____ _ __ ___ _  ___  _ __    ( _ )     _ __  _ __ _____   ___  __| | ___ _ __ ___ 
-#     | |/ _ \ '__| '__/ _` |  _/ _ \| '__| '_ ` _ \   \ \ / / _ \ '__/ __| |/ _ \| '_ \   / _ \/\  | '_ \| '__/ _ \ \ / / |/ _` |/ _ \ '__/ __|
-#     | |  __/ |  | | | (_| | || (_) | |  | | | | | |   \ V /  __/ |  \__ \ | (_) | | | | | (_>  <  | |_) | | | (_) \ V /| | (_| |  __/ |  \__ \
-#     |_|\___|_|  |_|  \__,_|_| \___/|_|  |_| |_| |_|    \_/ \___|_|  |___/_|\___/|_| |_|  \___/\/  | .__/|_|  \___/ \_/ |_|\__,_|\___|_|  |___/
-#                                                                                                   | |                                         
-#                                                                                                   |_|                                         
-
 terraform {
   required_version = ">= 1.1.7"
-  # cf. https://www.terraform.io/language/settings/backends/azurerm
   backend "azurerm" {
-    resource_group_name  = "RG-AKSCluster"         # mettre ici le nom du resource group de vos ressource
-    storage_account_name = "backendterrapierrc"      # mettre le nom du compte de stockage créer dans le lab 1
+    resource_group_name  = "RG-AKSCluster"
+    storage_account_name = "backendterrapierrc"
     container_name       = "tfstate"
-    key                  = "pipeline-gh.terraform.tfstate" 
-    # pour les options d'authentification au backend Azure Storage cf. https://www.terraform.io/language/settings/backends/azurerm
-    # sas_token = "sp=racwdli&st=2022-03-11T14:00:16Z&se=2022-03-11T22:26:16Z&spr=https&sv=2020-08-04&sr=c&sig=iiu05uUa%2Bh9tdqO%2F%2FwJEGZlkeJR5Ufgna1rshTaVv7s%3D"
-        
+    key                  = "pipeline-gh.terraform.tfstate"
   }
-  
+
   required_providers {
     azurerm = {
-      # The "hashicorp" namespace is the new home for the HashiCorp-maintained
-      # provider plugins.
-      #
-      # source is not required for the hashicorp/* namespace as a measure of
-      # backward compatibility for commonly-used providers, but recommended for
-      # explicitness.
-      # Configure the Azure Provider
-      # more info : https://github.com/terraform-providers/terraform-provider-azurerm
-      # Check Changelog : https://github.com/terraform-providers/terraform-provider-azurerm/blob/master/CHANGELOG.md
+
       source  = "hashicorp/azurerm"
       version = "= 2.98.0"
-
     }
-
+  }
 }
-}
 
-# Configure the Azure Provider
 provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   features {}
   subscription_id = var.AzureSubscriptionID
   client_id       = var.AzureClientID
