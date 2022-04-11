2e96effeda016b59679717443655325ca4d90f51
Pierre Chesne <pierrc@macbook-pro-de-pierre-1.home>
2022-04-11 14:57:06 +0200

    test fmt

--- Terraform/Vm/variables.tf
+++ Terraform/Vm/variables.tf
@@ -13,33 +13,33 @@ variable "azureRegion" {
 }
 
 variable "vnetName" {
-    type = string
-    default = "Vnet-VM"  
+  type    = string
+  default = "Vnet-VM"
 }
 
 variable "subnetName" {
-    type = string
-    default = "Subnet-VM"  
+  type    = string
+  default = "Subnet-VM"
 }
 
 variable "nicName" {
-    type = string
-    default = "Nic-1"
-  
+  type    = string
+  default = "Nic-1"
+
 }
 
 variable "vmName" {
-    type = string
-    default = "VM-Linux"  
+  type    = string
+  default = "VM-Linux"
 }
 
 # az vm list-skus -l westus
 variable "vmSize" {
-    type = string
-    default = "Standard_B2ms"  
+  type    = string
+  default = "Standard_B2ms"
 }
 
 variable "vmUser" {
-    type = string
-    default = "adminuser"  
+  type    = string
+  default = "adminuser"
 }
