b2ccc5d0bb52c47b5552c22eb10205c92187dd4e
Pierre Chesne <pierrc@macbook-pro-de-pierre-1.home>
2022-04-15 22:16:31 +0200

    runner

--- Terraform/Vm/main.tf
+++ Terraform/Vm/main.tf
@@ -61,7 +61,7 @@ resource "azurerm_linux_virtual_machine" "terra_vm" {
   source_image_reference {
     publisher = "Canonical"    # az vm image list --output table
     offer     = "UbuntuServer" # az vm image list --offer UbuntuServer --all --output table
-    sku       = "16.04-LTS"    # az vm image list-skus --location westus --publisher Canonical --offer UbuntuServer --output table
+    sku       = "18.04-LTS"    # az vm image list-skus --location westus --publisher Canonical --offer UbuntuServer --output table
     version   = "latest"
   }
 }
