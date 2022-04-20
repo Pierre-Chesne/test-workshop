330d5374cc2251909c1877f135e9d805d3d30d01
Pierre Chesne <pierrc@macbook-pro-de-pierre-1.home>
2022-04-20 11:00:18 +0200

    custom script

--- Terraform/Vm/output.tf
+++ Terraform/Vm/output.tf
@@ -1,3 +1,7 @@
 output "ip_public_vm" {
   value = azurerm_linux_virtual_machine.terra_vm.public_ip_address
 }
+
+output "virtual_machine_id" {
+  value = azurerm_linux_virtual_machine.terra_vm.id
+}
