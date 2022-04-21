21705bd2a5ace647cfba485476124a6952ceab45
Pierre Chesne <pierrc@macbook-pro-de-pierre-1.home>
2022-04-21 19:54:17 +0200

    custom script

--- Terraform/Runner/version.tf
+++ Terraform/Runner/version.tf
@@ -17,8 +17,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.AzureSubscriptionID
-  client_id       = var.AzureClientID
-  client_secret   = var.AzureClientSecret
-  tenant_id       = var.AzureTenandID
+
 }
