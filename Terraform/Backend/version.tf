14d1f559c265671b9f45cac9ad105e6bf49b3514
Pierre Chesne <pierrc@macbook-pro-de-pierre-1.home>
2022-04-22 19:30:43 +0200

    deploy backend

--- Terraform/Backend/version.tf
+++ Terraform/Backend/version.tf
@@ -1,9 +1,18 @@
 terraform {
   required_version = ">= 1.1.7"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "= 2.98.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.AzureSubscriptionID
+  client_id       = var.AzureClientID
+  client_secret   = var.AzureClientSecret
+  tenant_id       = var.AzureTenandID
 }
