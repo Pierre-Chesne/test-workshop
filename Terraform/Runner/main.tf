c68b1ec8e31a5039c7cd753c13978f3bd6f2146c
Pierre Chesne <pierrc@macbook-pro-de-pierre-1.home>
2022-04-22 14:51:45 +0200

    custom script

--- Terraform/Runner/main.tf
+++ Terraform/Runner/main.tf
@@ -1,3 +1,17 @@
+data "terraform_remote_state" "terra_datasource_vm" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = "RG-Backend-Terraform"
+    storage_account_name = "storagebeterraform"
+    container_name       = "tfstate"
+    key                  = "vm.terraform.tfstate"
+  }
+}
+
+
+
+
 resource "azurerm_resource_group" "terra_rg_test" {
   name = "rg-test"
   location = "westeurope"
