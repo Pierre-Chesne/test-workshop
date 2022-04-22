d324c929cf2a6458f315111e39c143866cf5faff
Pierre Chesne <pierrc@macbook-pro-de-pierre-1.home>
2022-04-22 20:01:47 +0200

    deploy backend

--- Terraform/ConfigRunner/main.tf
+++ Terraform/ConfigRunner/main.tf
@@ -23,18 +23,6 @@ provider "azurerm" {
   tenant_id       = var.AzureTenandID
 }
 
-data "terraform_remote_state" "terra_datasource_vm" {
-  backend = "azurerm"
-
-  config = {
-    resource_group_name  = "RG-Backend-Terraform"
-    storage_account_name = "storagebeterraform"
-    container_name       = "tfstate"
-    key                  = "vm.terraform.tfstate"
-  }
-}
-
-
 resource "azurerm_resource_group" "terra_rg_test" {
   name = "rg-test"
   location = "westeurope"
